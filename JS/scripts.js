226950d56db955de01dca1f9500c3dc97bc6517b
Luan0Avila <avila.almeida.luan@gmail.com>
2025-05-22 15:51:53 -0300

    começando a fazer o localStorage

--- JS/scripts.js
+++ JS/scripts.js
@@ -12,7 +12,7 @@ const filterBtn = document.querySelector("#filter-select")
 let oldInputValue;
 
 // Funções
-const saveTodo = (text) => {
+const saveTodo = (text, done = 0, save = 1) => {
 
     const todo = document.createElement("div")
     todo.classList.add("todo");
@@ -35,6 +35,15 @@ const saveTodo = (text) => {
     deleteBtn.classList.add("remove-todo");
     deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
     todo.appendChild(deleteBtn);
+
+    // Utilizando dados da localStorage
+    if (done) {
+        todo.classList.add("done");
+    };
+
+    if (save) {
+        saveTodoLocalStorage({text, done})
+    }
 
     todoList.appendChild(todo);
 
@@ -103,8 +112,8 @@ const filterTodos = (filterValue) => {
 
     default:
         break;
-    }
-}
+    };
+};
 
 // Eventos
 todoForm.addEventListener("submit", (e) => {
@@ -181,4 +190,23 @@ filterBtn.addEventListener("change", (e) => {
     const filterValue = e.target.value;
 
     filterTodos(filterValue);
-})
+});
+
+// Local Storage
+const getTodosLocalStorage = () => {
+    const todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+    return todos;
+}
+
+const saveTodoLocalStorage = (todo) => {
+    const todos = getTodosLocalStorage();
+
+    todos.push(todo)
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+    
+
+    //salva tudo na ls
+}
